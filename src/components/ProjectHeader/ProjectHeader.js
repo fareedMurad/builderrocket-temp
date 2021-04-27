8adddfc53140c971efd48b1c78adbc31aa64e0d3
Israel <i.gonzalez1291@gmail.com>
2021-04-26 19:26:42 -0700

    project header styling

--- src/components/ProjectHeader/ProjectHeader.js
+++ src/components/ProjectHeader/ProjectHeader.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import './ProjectHeader.scss';
 
 const ProjectHeader = () => {
 
     return (
         <div className='project-header'>
-            <Row>
-                <Col lg={4}>Hello</Col>
-                <Col lg={4}></Col>
-                <Col lg={4}></Col>
-            </Row>
+            {/* <Container> */}
+                <div className='d-flex ml-3'>
+                    <Col lg={4}>
+                        <div className='d-flex'>
+                            <Col>   
+                                <div className='project-image'>
+                                    <img alt='project' height='119' width='167' />
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className='text'>Project #</div>
+                                <div className='pt-3 project-name'>Project Name</div>
+                                <div>Address</div>
+
+                                <div className='pt-2'>
+                                    <a href='/' className='text'>Save as New Project</a>
+                                </div>
+                            </Col>
+                        </div>
+                    </Col>
+                    <Col lg={{ offset: 1, span: 3 }}>
+                        <div className='bold-text pt-5'>Customer Name</div>
+                        <div className='d-flex'>
+                            <div className='pr-2 text'>Phone</div>
+                            <div className='text'>Email Address</div>
+                        </div>
+                        <div className='d-flex pt-3'>
+                            <div className='bold-text pr-5'>Closed On</div>
+                            <div className='bold-text pl-5'>Build Time</div>
+                        </div>
+                    </Col>
+                    <Col lg={4}>
+                        <div className='d-flex pt-3 justify-content-end'>
+                            <div className='pr-3 text'>Status: <span className='bold-text'>Open Project</span></div>
+                            <button className='snapshot-btn'>Project Snapshot</button>
+                        </div>
+
+                        <div className='d-flex total justify-content-end text'>
+                            Project Total: $0.00 Margin: $0.00
+                        </div>
+                    </Col>
+                </div>
+            {/* </Container> */}
         </div>
     )
 }
---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,15 @@
+import {  } from '../actions/types';
+
+const intialState = {
+    project: {}
+};
+
+const projectReducer = (state = intialState, action) => {
+    switch (action.type) {
+     
+        default:
+            return state;
+    }
+}
+
+export default projectReducer;
