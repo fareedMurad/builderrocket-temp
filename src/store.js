6aa767afae032fc4957b285a22aa46570cb90b63
Israel <i.gonzalez1291@gmail.com>
2021-05-02 10:08:11 -0700

    Save redux store to local storage

--- src/store.js
+++ src/store.js
@@ -4,6 +4,29 @@ import rootReducer from './reducers';
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const saveToLocalStorage = (state) => {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem('persistantState', serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+      const serialisedState = localStorage.getItem('persistantState');
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+
+
+const store = createStore(rootReducer, loadFromLocalStorage(), applyMiddleware(...middleware));
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
