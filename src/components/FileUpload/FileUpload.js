01d28cf3022f30657b239efd6497008a7a85276b
Israel <i.gonzalez1291@gmail.com>
2021-05-11 14:13:34 -0700

    File upload input button to open file upload

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -2,19 +2,16 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import './FileUpload.scss';
 
-
 const FileUpload = (props) => {
     const { label } = props;
 
     return (
         <div className='file-upload'>
             <Form.Label className='input-label'>{label}</Form.Label>
-            
-            <Form.File
-                type='file'
-                className='custom-file-label'
-                custom
-            />
+
+            <label class='custom-file-label'>
+                <input type='file' />
+            </label>
         </div>
     )
 }
