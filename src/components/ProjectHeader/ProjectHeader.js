1b51cf1809660ef7b8cd94e23dd24a9c69fc679e
Israel <i.gonzalez1291@gmail.com>
2021-06-07 10:50:55 -0700

    Project Header #8

--- src/components/ProjectHeader/ProjectHeader.js
+++ src/components/ProjectHeader/ProjectHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import Utils from '../../utils';
 import './ProjectHeader.scss';
@@ -16,69 +16,92 @@ const ProjectHeader = () => {
 
     return (
         <div className='project-header'>
-            {/* <Container> */}
-                <div className='d-flex flex-wrap justify-content-around'>
-                        <div className='d-flex pt-2'>
-                            <Col>   
-                                <div className='project-image d-flex'>
-                                    <img 
-                                        alt='project' 
-                                        height='119' 
-                                        width='167' 
-                                        src={project?.ThumbnailURL}
-                                    />
-                                </div>
-                            </Col>
-                            <Col md={8}>
-                                <div className='text'>Project # {project?.ProjectNumber} </div>
+            <div className='d-flex flex-wrap justify-content-around'>
+                    <div className='d-flex pt-2'>
+                        <Col>   
+                            <div className='project-image d-flex'>
+                                <img 
+                                    alt='project' 
+                                    height='119' 
+                                    width='167' 
+                                    src={project?.ThumbnailURL}
+                                />
+                            </div>
+                        </Col>
+                        <Col md={8}>
+                            <div className='text'>
+                                {project?.ProjectNumber} 
+                                <i className='fas fa-share-square ml-5 share-icon'></i>    
+                            </div>
 
-                                <div className='pt-2 d-flex'>
-                                    <div className='project-name'>Project Name</div>   
-                                    <div className='text pl-2 pt-1'>{project?.ProjectName}</div>
-                                </div>
+                            <div className='pt-2 d-flex'>
+                                <div className='project-name'>{project?.ProjectName}</div>   
+                            </div>
 
-                                <div className='d-flex'>
-                                    Address
-                                    {'    '}
-                                    {project?.StreetAddress1}
-                                </div>
+                            <div className='d-flex'> 
+                                {project?.StreetAddress1}
+                            </div>
 
-                                <div className='pt-2'>
-                                    <a href='/' className='text'>Save as New Project</a>
+                            <div className='pt-1 d-flex'>
+                                <div className='add-btn'>
+                                    <Button 
+                                        variant='link' 
+                                        className='link-btn'
+                                    >
+                                        <i className='fas fa-file-download mr-2 save-icon'></i>
+                                        Save as New Project
+                                    </Button>
                                 </div>
-                            </Col>
-                        </div>
-
-                    <div>
-                        <div className='bold-text pt-5'>Customer Name</div>
-                        <div className='d-flex'>
-                            <div className='pr-2 text'>Phone</div>
-                            <div className='text'>Email Address {project?.Customers?.[0]?.email}</div>
-                        </div>
-                        <div className='d-flex pt-3'>
-                            <div className='bold-text pr-3'>
-                                Closed On 
-                                {'    '}
-                                {Utils.formatDateDashes (project?.CloseDate)}</div>
-                            <div className='bold-text pl-3'>Build Time</div>
-                        </div>
+                            </div>
+                        </Col>
                     </div>
 
-
-                    <div>
-                        <div className='d-flex pt-3 justify-content-end'>
-                            <div className='pr-3 text'>
-                                Status: <span className='bold-text'>{statusMap[project?.StatusID]}</span>
-                            </div>
-                            <button className='snapshot-btn'>Project Snapshot</button>
+                <div>
+                    <div className='bold-text pt-5'>
+                        {project?.Customers?.[0]?.FirstName}{' '}{project?.Customers?.[0]?.LastName}
+                    </div>
+                    <div className='d-flex'>
+                        <div className='pr-3 text phone'>
+                            <i className='fas fa-phone mr-2'></i>
+                            <a href={`tel:+1${project?.Customers?.[0]?.Phone}`}>
+                                {project?.Customers?.[0]?.Phone}
+                            </a>    
+                        </div>
+                        
+                        <div className='text email'>
+                            <i className='fas fa-envelope mr-2'></i> 
+                            <a href={`mailto:${project?.Customers?.[0]?.Email}`}> 
+                                {project?.Customers?.[0]?.Email}
+                            </a>
+                        </div>
+                    </div>
+                    <div className='d-flex pt-3'>
+                        <div className='bold-text pr-3'>
+                            Closed On 
+                            {'    '}
+                            {Utils.formatDateDashes (project?.CloseDate)}
                         </div>
 
-                        <div className='d-flex total justify-content-end text'>
-                            Project Total: $0.00 Margin: $0.00
-                        </div>
+                        {(project?.StatusID !== 2 && project?.BuildTime === null) &&
+                            <div className='bold-text pl-3'>Build Time</div>
+                        }
                     </div>
                 </div>
-            {/* </Container> */}
+
+
+                <div>
+                    <div className='d-flex pt-3 justify-content-end'>
+                        <div className='pr-3 text'>
+                            Status: <span className='bold-text'>{statusMap[project?.StatusID]}</span>
+                        </div>
+                        <button className='snapshot-btn'>Project Snapshot</button>
+                    </div>
+
+                    <div className='d-flex total justify-content-end text'>
+                        Project Total: $0.00 Margin: $0.00
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
