8f39453d4f0237d7ddba5a01612442e8a67fb37f
Israel <i.gonzalez1291@gmail.com>
2021-06-10 09:46:16 -0700

    Login updates - clean up form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button, Form, Spinner } from 'react-bootstrap';
 import { loginEmailPassword } from '../../actions/authActions';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { isEmpty } from 'lodash'
 
 const Login = (props) => {
-    const { history } = props;
-
     const dispatch = useDispatch();
-
-    const token = useSelector(state => state.auth.token);
 
     const [login, setLogin] = useState({ email: 'ronbibb@gmail.com' });
     const [isLoading, setIsLoading] = useState(false);
@@ -24,31 +20,30 @@ const Login = (props) => {
                 });
     }
 
-    useEffect(() => {
-        if (token) 
-            history.push('/');
-    })
-
     return (
         <Row className='justify-content-center pt-5'>
             <Col md={4}>
                 <h2>Login</h2>
                 <br/>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    value={login?.email}
-                    placeholder='Email'
-                    type='email'
-                    onChange={(e) => setLogin({ ...login, email: e.target.value })}
-                />
-                <br />
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    placeholder='Password'
-                    type='password'
-                    onChange={(e) => setLogin({ ...login, password: e.target.value })}
-                />
-                <div className='pt-4'> 
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type='email'
+                            placeholder='Email'
+                            value={login?.email}
+                            onChange={(e) => setLogin({ ...login, email: e.target.value })}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            autoComplete='true'
+                            placeholder='Password'
+                            onChange={(e) => setLogin({ ...login, password: e.target.value })}
+                        />
+                    </Form.Group>
                     {isLoading ? 
                         <div className='d-flex justify-content-center'>
                             <Spinner 
@@ -59,7 +54,7 @@ const Login = (props) => {
                     :
                         <Button onClick={handleLogin}>Login</Button>
                     }
-                </div>
+                </Form>
             </Col>
         </Row>
     )
