e9fe2953babcae206324cfbcd7bc334be7578078
fareedMurad <fareedMurad>
2022-01-21 06:49:19 +0500

    Update report tab

---
+++ src/components/PDF/index.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import ReactToPrint from "react-to-print";
+import { Button } from 'react-bootstrap';
+
+export const CustomPrinter = React.forwardRef((props, ref) => {
+
+    const onBeforeGetContentResolve = React.useRef(null);
+
+    const handleAfterPrint = React.useCallback(() => {
+
+    }, []);
+
+    const handleBeforePrint = React.useCallback(() => {
+        console.log("`onBeforePrint` called");
+    }, []);
+
+    const handleOnBeforeGetContent = React.useCallback(() => {
+
+        return new Promise((resolve) => {
+            onBeforeGetContentResolve.current = resolve;
+
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    }, []);
+
+    React.useEffect(() => {
+        if (
+            typeof onBeforeGetContentResolve.current === "function"
+        ) {
+            onBeforeGetContentResolve.current();
+        }
+    }, [onBeforeGetContentResolve.current]);
+
+    const reactToPrintContent = React.useCallback(() => {
+        return ref.current;
+    }, [ref.current]);
+
+    const reactToPrintTrigger = React.useCallback(() => {
+        return <Button variant='link' className='link-btn' >
+            <i className={props.icon}></i>
+            {props.title}
+        </Button>;
+    }, []);
+
+    return (
+        <ReactToPrint
+            copyStyles={true}
+            content={reactToPrintContent}
+            documentTitle="AwesomeFileName"
+            onAfterPrint={handleAfterPrint}
+            onBeforeGetContent={handleOnBeforeGetContent}
+            onBeforePrint={handleBeforePrint}
+            removeAfterPrint
+            trigger={reactToPrintTrigger}
+        />
+    );
+});
